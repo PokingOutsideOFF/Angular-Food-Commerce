6bc3c0d41a1718e6b130301caa744dad94ac66d6
Sabale <ssabale@deloitte.com>
2024-12-13 17:01:21 +0530

    Header Footer

--- Online/src/app/app.module.ts
+++ Online/src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PagesModule } from './pages/pages.module';
@@ -13,7 +12,7 @@ import { ComponentsModule } from './components/components.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    PagesModule,
+
     ComponentsModule
   ],
   providers: [],
