cda166ddf75c42034aa4344844d2b2615b4d2cfd
Sabale <ssabale@deloitte.com>
2024-12-16 17:40:37 +0530

    Local storage

--- Online/src/app/pages/profile/profile.component.ts
+++ Online/src/app/pages/profile/profile.component.ts
@@ -7,5 +7,19 @@ import { Component } from '@angular/core';
   standalone: false
 })
 export class ProfileComponent {
+  una:string='';
+  pwd:string='';
+  ema:string='';
+  tel:string='';
+  constructor(){
+    var uname =localStorage.getItem("uname");
+    var pass =localStorage.getItem("pass");
+    var email =localStorage.getItem("email");
+    var tel =localStorage.getItem("tel");
+    this.una=`${uname}`;
+    this.pwd=`${pass}`;
+    this.una=`${email}`;
+    this.una=`${tel}`;
+  }
 
 }
