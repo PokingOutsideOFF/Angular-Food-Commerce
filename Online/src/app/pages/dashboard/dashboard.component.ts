7d1459ce367d49fb728c02305b1f90b2fd9bb019
s-cube-15 <2020.sudhanshu.sabale@ves.ac.in>
2024-12-17 12:11:19 +0530

    dashboard updates and icon

--- Online/src/app/pages/dashboard/dashboard.component.ts
+++ Online/src/app/pages/dashboard/dashboard.component.ts
@@ -9,18 +9,32 @@ import { ActivatedRoute } from '@angular/router';
   standalone: false
 })
 export class DashboardComponent {
-  username: string = ""
-  tel:number = 0;
-  email:string = ""
-  constructor(private route: ActivatedRoute){  }
+
+  una:string='';
+  pwd:string='';
+  ema:string='';
+  tel:string='';
+  constructor(){
+    var uname =localStorage.getItem("uname");
+    // var pass =localStorage.getItem("pass");
+    // var email =localStorage.getItem("email");
+    // var tel =localStorage.getItem("tel");
+    this.una=`${uname}`;
+    // this.pwd=`${pass}`;
+    // this.ema=`${email}`;
+    // this.tel=`${tel}`;
+  // username: string = ""
+  // tel:number = 0;
+  // email:string = ""
+  // constructor(private route: ActivatedRoute){  }
   
-  ngOnInit() {
-    this.route.params.subscribe(params => {
-      console.log('Params:', params); 
-      this.username = params['user'];
-      this.tel = params['tel'];
-      this.email = params['email']
-    }); 
+  // ngOnInit() {
+  //   this.route.params.subscribe(params => {
+  //     console.log('Params:', params); 
+  //     this.username = params['user'];
+  //     this.tel = params['tel'];
+  //     this.email = params['email']
+  //   }); 
+  // }
   }
-  
 }
