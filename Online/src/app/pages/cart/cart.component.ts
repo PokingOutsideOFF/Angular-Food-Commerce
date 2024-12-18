1e91e641886583a1d4722d8c8f2349ccbdca2639
s-cube-15 <2020.sudhanshu.sabale@ves.ac.in>
2024-12-18 17:45:52 +0530

    Dynamic

--- Online/src/app/pages/cart/cart.component.ts
+++ Online/src/app/pages/cart/cart.component.ts
@@ -1,20 +1,19 @@
 import { HttpClient } from '@angular/common/http';
-import { Component } from '@angular/core';
-import { OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
   styleUrls: ['./cart.component.css'],
   standalone: false
-
 })
-export class CartComponent {
-  cart: any[] = []
-  item_price: number = 0
-  total: number = 0
-  rate: number = 0
+export class CartComponent implements OnInit {
+  cart: any[] = [];
+  item_price: number = 0;
+  total: number = 0;
+  rate: number = 0;
 
-  url = "http://localhost:3000/cart"
+  url = "http://localhost:3000/cart";
 
   constructor(private _http: HttpClient) {
     this._http.get<any[]>(this.url)
@@ -23,67 +22,52 @@ export class CartComponent {
         console.log(this.cart);
         this.calculateTotal();
         this.calculatesubtotal();
-      }
-      );
+      });
   }
-  ngOnInit() {
 
-  }
+  ngOnInit() {}
 
   calculateTotal() {
-    console.log("Here")
+    console.log("Here");
+    this.total = 0; // Reset total before recalculating
     for (let i of this.cart) {
-      this.total += i.price;
+      this.total += i.price * i.quantity;
     }
   }
 
-
-
   calculatesubtotal(item_id: any = "b188") {
-    console.log("Here")
+    console.log("Here");
     const item = this.cart.find(item => item.id === item_id);
-    this.item_price = item.price;
-    this.rate = item.price
-
+    if (item) {
+      this.item_price = item.price;
+      this.rate = item.price;
+    }
   }
 
   increaseQuantity(item_id: any, increment: number = 1) {
     var it = this.cart.find(item => item.id === item_id);
-    console.log(it)
+    console.log(it);
     if (it) {
-      it.quantity = it.quantity + 1;
-      // it.price = this.rate * it.quantity;
-      this.total = 0
-      for (let i of this.cart) {
-        this.total += (i.price * i.quantity);
-      }
-
+      it.quantity = it.quantity + increment;
+      this.calculateTotal();
     }
-
   }
+
   decreaseQuantity(item_id: any, decrement: number = 1) {
     var it = this.cart.find(item => item.id === item_id);
-    this.rate = it.price
-    console.log("decrease called")
-
-    if (it) {
+    console.log("decrease called");
+    if (it && it.quantity > 1) {
       it.quantity -= decrement;
-      // it.price = this.rate * it.quantity
-      this.total = 0
-      for (let i of this.cart) {
-        this.total += (i.price * it.quantity);
-      }
+      this.calculateTotal();
     }
   }
 
-
   removeItem(item_id: any) {
-
     this._http.delete<any[]>(`${this.url}/${item_id}`)
-      .subscribe(resp=>this.cart = resp);
-
+      .subscribe(resp => {
+        // Update the local cart state after successful deletion
+        this.cart = this.cart.filter(item => item.id !== item_id);
+        this.calculateTotal(); // Recalculate the total after removing the item
+      });
   }
-
 }
-
-
